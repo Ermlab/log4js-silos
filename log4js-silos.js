da0c20c6809b4d8a9fdbffdf645153e3bb1f7fbc
pgorecki <przemyslaw.gorecki@gmail.com>
2014-08-08 14:46:51 +0200

    Added enableSilos() method

--- log4js-silos.js
+++ log4js-silos.js
@@ -2,8 +2,8 @@ log4js = Npm.require("log4js");
 silosAppender = Npm.require("log4js-appender-silos");
 
 
-log4js.enableSilos = function (token) {
+log4js.enableSilos = function (token, host, port) {
     var appender = 'log4js-appender-silos';
     log4js.loadAppender(appender);
-    log4js.addAppender(log4js.appenders[appender](token));
+    log4js.addAppender(log4js.appenders[appender](token, host, port));
 }
